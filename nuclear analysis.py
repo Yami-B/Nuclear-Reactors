435512005cc54e83be321f6863976ece1edc4fc9
Yami-B <59410079+Yami-B@users.noreply.github.com>
2020-01-29 23:16:49 +0100

    Update nuclear analysis.py

--- nuclear analysis.py
+++ nuclear analysis.py
@@ -97,11 +97,128 @@ plt.show()
 #than the others (smaller variance in means of capacity factors)
 
 
+ro = pd.read_csv('C:\\Users\\Yasta\\Desktop\\project2\\reactors-operating 1.csv') 
+NRC1=[]
+NRC2=[]
+NRC3=[]
+NRC4=[]
+NRC_matrix=[NRC1,NRC2,NRC3,NRC4]
+for i in range(0,ro.shape[0]):
+    if i+1 not in nodes_to_remove: 
+       NRC_matrix[ro.loc[i,'NRC Region']-1].append(cf[str(i+1)])
 
-######
-#Automatically forecasting for each time series would be time consuming since 
-#some of them are non stationary and would therefore specific types of models (like ARIMA
-#or random  walk) and others not. Dealing with very short time series is not helping either.
-#What we suggest here is to fit one general model to all the time series as it would increase the number
-#of data and therefore the reliability of the forecast
-     
+
+def Mean(A):
+    B=[]
+    for i in range(0,len(A)):
+        B.append(A[i].mean())
+    return(B)
+NRC1_mean=Mean(NRC1)
+NRC2_mean=Mean(NRC2)
+NRC3_mean=Mean(NRC3)
+NRC4_mean=Mean(NRC4)
+
+from scipy.stats import levene
+W,p_value=levene(NRC1_mean,NRC2_mean,NRC3_mean,NRC4_mean)
+print(W)
+print(p_value)
+#Hypothesis that the variances are equal can't be rejected and each sample has more than 5 individuals
+#(p_value=0.45>0.05 ==> H0 failed to be rejected)
+#All assumptions for Kruskal are respected
+
+from scipy.stats import kruskal
+H,p_value=kruskal(NRC1_mean,NRC2_mean,NRC3_mean,NRC4_mean)
+print(H)   
+print(p_value)  
+#p_value=0.16 > 0.05, it means the samples have supposedly the same medians
+#The plants don't differ significantly depending of NRC regions
+
+import pylab
+BoxName = ['NRC1','NRC2','NRC3','NRC4']
+data = [NRC1_mean,NRC2_mean,NRC3_mean,NRC4_mean]
+pylab.xticks([1,2,3,4], BoxName)
+plt.boxplot(data)
+plt.show()
+
+
+
+
+from statsmodels.tsa.stattools import grangercausalitytests
+from pandas import Series
+
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return Series(diff)
+#difference time series in order to be able to apply VAR fitting on stationary time series
+
+date_rng = pd.date_range(start='1/2008', end='1/2019', freq='Y')
+df=cf.iloc[::-1]
+df['Date'] = pd.to_datetime(date_rng)
+df.set_index(df['Date'],inplace=True)
+df.drop(['Date'],inplace=True,axis=1)
+upsampled = df.resample('M')
+interpolated = upsampled.interpolate(method='linear')
+#Time indexed the dataframe containing the capacity factors following years and 
+#interpolated the points in order to have longer time series for forecasting
+
+
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import kpss
+from statsmodels.tsa.api import VAR
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+ts1=interpolated['7']
+ts2=interpolated['9']
+RMSE=[]
+RMSE1=[]
+x=[]
+#for i in range(70,110):
+result1 = kpss(ts1)[1]
+result2 = kpss(ts2)[1]
+result3 = adfuller(ts1)[1]
+result4 = adfuller(ts2)[1]
+while(result1 >= 0.05 and result2 >= 0.05 and result4 <=0.05 and result3 <=0.05):
+    ts1=difference(ts1)
+    ts2=difference(ts2)
+    result1 = kpss(ts1)[1]
+    result2 = kpss(ts2)[1]
+    result3 = adfuller(ts1)[1]
+    result4 = adfuller(ts2)[1]
+    print('NOK')
+data=pd.DataFrame()
+data['ts1']=ts1
+data['ts2']=ts2
+training_data=data.iloc[0:95,:]
+from statsmodels.tsa.ar_model import AR
+model = VAR(training_data)
+model2=AR(ts2.iloc[0:95])
+results = model.fit(maxlags=5, ic='aic')
+results2 = model2.fit()
+predictions = results2.predict(start=95, end=120, dynamic=False)
+#results.plot_forecast(26)
+#results2.plot_forecast(26)
+error = mean_squared_error(ts2.iloc[95:], predictions)
+print(error)
+y_predicted=results.forecast(data.values[-5:], 121-95)
+y_actual=data.iloc[95:,:]
+rms = sqrt(mean_squared_error(y_actual.values[:,0], y_predicted[:,0]))
+rms1 = sqrt(mean_squared_error(y_actual.values[:,1], y_predicted[:,1]))
+print(rms1)
+
+RMSE.append(rms)
+RMSE1.append(rms1)
+x.append(i)
+plt.plot(x[5:],RMSE[5:])
+plt.plot(x,RMSE1)
+
+#Take the dependent time series and fit a Vector Autoregressive Model
+#in order to improve the forecasts (since some time series are dependent:
+#lagged values of one time serie might give information on future values of another time serie)
+
+#Next we'll fit ARIMA model to independent plants' capacity factors' evolution in time
+
+
